c45f12b2184bbf852fc8b4ac96f254afffb0c1b1
Gaurav Bansal <gauravkbansal@gmail.com>
2016-12-27 15:56:24 -0500

    Linear Regression - sklearn

--- code/outliers/outlier_cleaner.py
+++ code/outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -12,9 +12,19 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
     ### your code goes here
-
+    for idx, val in np.ndenumerate(ages):
+        error = residual_error(predictions[idx], net_worths[idx])
+        cleaned_data.append((ages[idx], net_worths[idx],error))
+        
     
+    cleaned_data.sort(key=lambda row: row[2])
+    size = len(cleaned_data)
+    print("size before slicing %d", size)
+    slice_len = int(0.9 * size)
+    print("sliced array len %d", slice_len)
+    cleaned_data = cleaned_data[:slice_len]
     return cleaned_data
 
+def residual_error(prediction, net_worth):
+    return ((prediction - net_worth)**2)
--- code/outliers/outlier_removal_regression.py
+++ code/outliers/outlier_removal_regression.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 
-import random
 import numpy
 import matplotlib.pyplot as plt
 import pickle
+import sys
+import os
+sys.path.append(os.getcwd())
 
 from outlier_cleaner import outlierCleaner
 
-
 ### load up some practice data with outliers in it
-ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
-net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
-
-
+ages = pickle.load( open("practice_outliers_ages.pkl", "rb") )
+net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "rb"))
 
 ### ages and net_worths need to be reshaped into 2D numpy arrays
 ### second argument of reshape command is a tuple of integers: (n_rows, n_columns)
@@ -24,18 +23,13 @@ from sklearn.cross_validation import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
 ### fill in a regression here!  Name the regression object reg so that
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print("slope::", reg.coef_)
+print("intercept::", reg.intercept_)
+print("score", reg.score(ages_test,net_worths_test))
 ### the plotting code below works, and you can see what your regression looks like
-
-
-
-
-
-
-
-
-
-
-
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
 except NameError:
@@ -50,15 +44,8 @@ try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
 except NameError:
-    print "your regression object doesn't exist, or isn't name reg"
-    print "can't make predictions to use in identifying outliers"
-
-
-
-
-
-
-
+    print ("your regression object doesn't exist, or isn't name reg")
+    print ("can't make predictions to use in identifying outliers")
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -66,19 +53,21 @@ if len(cleaned_data) > 0:
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 
     ### refit your cleaned data!
+    print("After outlier removal")
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print("slope ", reg.coef_)
+        print("intercept ", reg.intercept_)
+        print("score", reg.score(ages_test,net_worths_test))
     except NameError:
-        print "you don't seem to have regression imported/created,"
-        print "   or else your regression object isn't named reg"
-        print "   either way, only draw the scatter plot of the cleaned data"
+        print ("you don't seem to have regression imported/created,")
+        print ("   or else your regression object isn't named reg")
+        print ("   either way, only draw the scatter plot of the cleaned data")
     plt.scatter(ages, net_worths)
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
 
-
 else:
-    print "outlierCleaner() is returning an empty list, no refitting to be done"
-
+    print ("outlierCleaner() is returning an empty list, no refitting to be done")
