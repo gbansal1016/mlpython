83284f42cbaf4ad47b5165bc9034a0dd49c199c2
Gaurav Bansal <gauravkbansal@gmail.com>
2016-12-27 13:21:23 -0500

    Regression changes

--- code/datasets_questions/explore_enron_data.py
+++ code/datasets_questions/explore_enron_data.py
@@ -16,7 +16,52 @@
 """
 
 import pickle
+import sys
+import math
+sys.path.append("../final_project/")
+from  poi_email_addresses import poiEmails
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+#keys = enron_data.keys()
+total=0
+count=0
+quantified_sal=0
+total_pym_nan_cnt=0
+for key, value in enron_data.items():
+    total = total+1
+    if (value["poi"]):
+        count = count + 1
+    if(not(math.isnan(float(value["salary"])))):
+        quantified_sal = quantified_sal + 1
+    if(math.isnan(float(value["total_payments"]))):
+        total_pym_nan_cnt = total_pym_nan_cnt + 1
+            
+print("total", total)
+print("Number of POI: %d" , count)
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print("Nan payment counts ids %d", total_pym_nan_cnt)
+print("Quantified salary %d", quantified_sal)
+      
+print("Email addresses of all POI %d",len(poiEmails()))          
+
+text_file = open("../final_project/poi_names.txt", "r")
+lines = text_file.readlines()
+count=0
+for line in lines:
+    if((len(line) > 1) and line[1]=='y'):
+        count = count + 1
+        
+print("Number of POI: %d" , count)      
+
+print("Stock value for Mr. James Prentice %f", enron_data["PRENTICE JAMES"]["total_stock_value"])
+print("Stock value for Mr. Wesley Colwell %d", enron_data["Colwell Wesley".upper()]["from_messages"])
+print("Stock options exercised by Mr. Skilling %d", enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+
+print("Total Payments to Mr. Skilling %d", enron_data["SKILLING JEFFREY K"]["total_payments"])
+print("Total Payments to  Mr. Lay %d", enron_data["LAY KENNETH L"]["total_payments"])
+print("Total Payments to  Mr. Fastow %d", enron_data["FASTOW ANDREW S"]["total_payments"])
+
+text_file.close()
 
 
+
+#count[1 for d in enron_data if d.get('poi')==1]
--- code/regression/finance_regression.py
+++ code/regression/finance_regression.py
@@ -17,19 +17,20 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
-
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 ### list the features you want to look at--first item in the 
-### list will be the "target" feature
+    ### list will be the "target" feature
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+len(data)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,12 +39,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+reg=LinearRegression()
+reg.fit(feature_train, target_train) 
 
+print("coefficient:", reg.coef_)
+print("intercept:", reg.intercept_)
 
-
-
-
-
+print("R squared on training data" , reg.score(feature_train, target_train))
+print("R squared on test data" , reg.score(feature_test, target_test))
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
