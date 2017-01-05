6df3aa58e28a27f83f8333aebe1013ba32a19fd0
Gaurav Bansal <gauravkbansal@gmail.com>
2017-01-05 17:16:09 -0500

    Logistic Classifier for image

---
+++ deep learning/code/setup.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  1 19:20:15 2017
+
+@author: gbans6
+"""
+
+
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+# Config the matplotlib backend as plotting inline in IPython
+num_classes = 10
+np.random.seed(133)
+
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None        
+
+dir_name = 'C:\\Users\\gbans6\\gbansalmba\\mlearning\\mlpython\\data\\nminst\\'
+
+
+def download_progress_hook(count, blockSize, totalSize):
+  """A hook to report the progress of a download. This is mostly intended for users with
+  slow internet connections. Reports every 1% change in download progress.
+  """
+  global last_percent_reported
+  percent = int(count * blockSize * 100 / totalSize)
+
+  if last_percent_reported != percent:
+    if percent % 5 == 0:
+      sys.stdout.write("%s%%" % percent)
+      sys.stdout.flush()
+    else:
+      sys.stdout.write(".")
+      sys.stdout.flush()
+      
+    last_percent_reported = percent
+
+def maybe_download(dir_name, filename, expected_bytes, force=False):
+  """Download a file if not present, and make sure it's the right size."""
+  if force or not os.path.exists(os.path.join(dir_name,filename)):
+    print('Attempting to download:', filename) 
+    filename, _ = urlretrieve(url + filename, os.path.join(dir_name,filename), reporthook=download_progress_hook)
+    print('\nDownload Complete!')
+  statinfo = os.stat(os.path.join(dir_name,filename))
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', os.path.join(dir_name,filename))
+  else:
+    raise Exception(
+      'Failed to verify ' + os.path.join(dir_name,filename)+ '. Can you get to it with a browser?')
+  return filename
+
+  
+def maybe_extract(filename, force=False):
+  cwd = os.getcwd()
+  os.chdir(dir_name)  
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  print('qualified_name:',filename)
+  if not os.path.exists(root):
+      os.makedirs(root)
+    
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  os.chdir(cwd)  
+  return data_folders
+
+def train_test_files():
+    train_filename = maybe_download(dir_name,'notMNIST_large.tar.gz', 247336696)
+    test_filename = maybe_download(dir_name,'notMNIST_small.tar.gz', 8458043)
+    return train_filename,test_filename
+
+#train_folders = maybe_extract(train_filename)
+#test_folders = maybe_extract(test_filename)  
